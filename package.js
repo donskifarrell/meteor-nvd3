d87db9dbbf7f728774662b51d1a292da6300f3aa
donskifarrell <donskifarrell+git@googlemail.com>
2014-01-22 01:36:43 +0000

    Added v1.1.15b version of NVD3

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-  summary: 'Re-usable charts for d3.js'
+  summary: 'Re-usable charts for d3.js - v1.1.15b'
 });
 
 Package.on_use(function (api) {
   api.use('d3', 'client');
   api.add_files([
     'lib/nv.d3.min.js',
-    'lib/nv.d3.css'
+    'lib/nv.d3.min.css'
   ], 'client');
 });
